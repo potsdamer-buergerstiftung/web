fc2adf64eb3674a8888f1e5ebed89404cc7d162c
Noël Sigmunczyk <65312133+linlogge@users.noreply.github.com>
2023-01-15 23:38:53 +0100

    remove unused title attr

--- apps/website/app/head.tsx
+++ apps/website/app/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
   return (
     <>
       <title>Potsdamer Bürgerstiftung: Brücken bauen, Menschen verbinden</title>
-      <DefaultHead title="Willkommen" />
+      <DefaultHead />
     </>
   );
 }
