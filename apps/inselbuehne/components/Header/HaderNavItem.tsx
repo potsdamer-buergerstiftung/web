9aa9e14ec6a4e6952e8243bf8659b317858753ba
Noël Sigmunczyk <65312133+linlogge@users.noreply.github.com>
2023-09-04 21:51:05 +0200

    fix mobile menu

--- apps/inselbuehne/components/Header/HaderNavItem.tsx
+++ apps/inselbuehne/components/Header/HaderNavItem.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import clsx from "clsx";
+import { useAtom } from "jotai";
 import Link from "next/link";
+import { mobileMenuOpen } from "./state";
 
 interface HeaderNavItemProps {
     isActive?: boolean;
@@ -14,9 +16,10 @@ const HeaderNavItem: React.FC<HeaderNavItemProps> = ({
     label,
     href,
 }) => {
+    const [open, setOpen] = useAtom(mobileMenuOpen);
 
     return (
-        <Link href={href} className="relative px-2 py-3 text-xl transition hover:text-green-500 lg:p-2 lg:text-lg">
+        <Link href={href} className="relative px-2 py-3 text-xl transition hover:text-green-500 lg:p-2 lg:text-lg" onClick={() => setOpen(false)}>
             {label}
         </Link>
     );
