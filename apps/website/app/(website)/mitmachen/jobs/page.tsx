d549a866d71321e75d3002b2bf4ef8825c14e571
Noël Sigmunczyk <65312133+linlogge@users.noreply.github.com>
2023-10-09 15:08:26 +0200

    rewrite jobs page

--- apps/website/app/(website)/mitmachen/jobs/page.tsx
+++ apps/website/app/(website)/mitmachen/jobs/page.tsx
@@ -4,24 +4,30 @@ import PageTitle from "@components/PageTitle";
 import Link from "next/link";
 
 export default function JobsPage() {
-    return (
-        <>
-            <PageTitle
-                title="Jobs"
-                breadcrumb={
-                    <PageBreadcrumb
-                        items={[
-                            <PageBreadcrumbItem label="Mitmachen" href="/mitmachen" />,
-                            <PageBreadcrumbItem label="Jobs" />,
-                        ]}
-                    />
-                }
-            />
-            <section className="pt-8 pb-16 container mx-auto maxw-4xl px-4">
-                Wir sind immer an Menschen, die mit uns ihr freiwilliges soziales Jahr
-                absolvieren möchten oder sich für Bundesfreiwilligendienst bereit erklären, interessiert.
-                Falls Du Interesse hast, <Link href="/kontakt" className="font-bold text-emerald-500">melde Dich gerne bei uns</Link>.
-            </section>
-        </>
-    );
+  return (
+    <>
+      <PageTitle
+        title="Jobs"
+        breadcrumb={
+          <PageBreadcrumb
+            items={[
+              <PageBreadcrumbItem label="Mitmachen" href="/mitmachen" />,
+              <PageBreadcrumbItem label="Jobs" />,
+            ]}
+          />
+        }
+      />
+      <section className="pt-8 pb-16 container mx-auto maxw-4xl px-4">
+        Wir sind immer auf der Suche nach neuen Talenten, die uns bei unserer
+        Arbeit unterstützen. Dafür suchen wir Werkstudierende und Menschen, die
+        bei uns ihr freiwilliges soziales Jahr, kulturelles Jahr, ökologisches
+        Jahr oder Bundesfreiwilligendienst absolvieren möchten. Falls Du
+        Interesse hast,{" "}
+        <Link href="/kontakt" className="font-bold text-emerald-500">
+          melde Dich gerne bei uns
+        </Link>
+        .
+      </section>
+    </>
+  );
 }
