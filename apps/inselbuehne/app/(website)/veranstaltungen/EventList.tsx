9bf02f306919f0bb587a2fb9545265a9d9d1a023
Noël Sigmunczyk <noel.sigmunczyk@burmester.de>
2024-06-06 17:40:38 +0200

    Fix timezone

--- apps/inselbuehne/app/(website)/veranstaltungen/EventList.tsx
+++ apps/inselbuehne/app/(website)/veranstaltungen/EventList.tsx
@@ -18,7 +18,7 @@ export default async function EventList({ promise }: EventListProps) {
         <React.Fragment>
             {events?.map((event: any) => (
                 //<h1>{event.start} {event.name}</h1>
-                <EventCard title={event.title} start={new Date(new Date(event.dateAndTimeSettings.startDate).setHours(new Date(event.dateAndTimeSettings.startDate).getHours() - 2))} summary={event.shortDescription} image={event.mainImage} localImage={false} href={`https://potsdamer-buergerstiftung.org/aktuelles/veranstaltungen/${event.slug}`} />
+                <EventCard title={event.title} start={new Date(event.dateAndTimeSettings.startDate)} summary={event.shortDescription} image={event.mainImage} localImage={false} href={`https://potsdamer-buergerstiftung.org/aktuelles/veranstaltungen/${event.slug}`} />
             ))}
         </React.Fragment>
     );
