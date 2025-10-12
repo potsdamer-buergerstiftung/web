984686fa05a2b9065ee109565916bb370663bb40
Noël Sigmunczyk <65312133+linlogge@users.noreply.github.com>
2025-10-12 23:33:03 +0200

    apply sync

--- apps/portal/entrypoint.sh
+++ apps/portal/entrypoint.sh
@@ -2,5 +2,5 @@
 
 npx directus bootstrap
 # Comment out the following line if you want to sync the snapshot
-# npx directus schema apply --yes ./snapshots/snapshot.yaml
+npx directus schema apply --yes ./snapshots/backup.yaml
 node cli.js bootstrap && pm2-runtime start ecosystem.config.cjs
