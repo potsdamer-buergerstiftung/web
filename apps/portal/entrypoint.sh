ed0e0c5d68c56b37d2e2890d3f91dd9ef6c11ec0
Noël Sigmunczyk <65312133+linlogge@users.noreply.github.com>
2025-10-13 13:05:46 +0200

    Add basic schema and install tree view

--- apps/portal/entrypoint.sh
+++ apps/portal/entrypoint.sh
@@ -2,5 +2,5 @@
 
 npx directus bootstrap
 # Comment out the following line if you want to sync the snapshot
-npx directus schema apply --yes ./snapshots/bu.json
+# npx directus schema apply --yes ./snapshots/bu.json
 node cli.js bootstrap && pm2-runtime start ecosystem.config.cjs
