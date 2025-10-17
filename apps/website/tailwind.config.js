08c5cbd07e3ae40342325c145ee2208f063583da
Emdee89 <20860575+Emdee89@users.noreply.github.com>
2025-10-17 22:17:03 +0200

    Remove mention of tailwind/line-clamp plugin

--- apps/website/tailwind.config.js
+++ apps/website/tailwind.config.js
@@ -17,7 +17,6 @@ module.exports = {
   plugins: [
     require('@tailwindcss/typography'),
     require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/forms')
   ],
 }
