f61346d0df34d3b4d5d800a745640d3a86032aa4
Noël Sigmunczyk <65312133+linlogge@users.noreply.github.com>
2025-10-17 21:52:57 +0200

    Upgrade to new directus sdk

--- apps/links/routes/[...link].ts
+++ apps/links/routes/[...link].ts
@@ -1,12 +1,21 @@
-import { Directus } from "@directus/sdk";
+import { createDirectus, readItems, rest, staticToken } from "@directus/sdk";
 
-const api = new Directus<any>("https://portal.potsdamer-buergerstiftung.org");
+const directus = createDirectus("https://portal.potsdamer-buergerstiftung.org").with(rest()).with(staticToken(process.env.DIRECTUS_TOKEN || ""));
 
 export default eventHandler(async (event) => {
   const id = event.context.params.link;
+  
   if (!id) {
     return "No Link specified";
   }
-  const link = await api.items("public_links").readOne(id);
-  return sendRedirect(event, link.destination, 307);
+
+  const link = await directus.request(readItems("public_links", {
+    filter: {
+      id: {
+        _eq: id,
+      },
+    },
+  }));
+
+  return sendRedirect(event, link[0]?.destination || "", 307);
 });
