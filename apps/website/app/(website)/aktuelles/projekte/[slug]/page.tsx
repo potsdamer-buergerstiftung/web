f06ed25d2ca940b33ac3975c92386e5af0587571
Noël Sigmunczyk <65312133+linlogge@users.noreply.github.com>
2026-03-19 16:43:57 +0100

    Show posts of projects after content

--- apps/website/app/(website)/aktuelles/projekte/[slug]/page.tsx
+++ apps/website/app/(website)/aktuelles/projekte/[slug]/page.tsx
@@ -1,9 +1,9 @@
 import { Metadata, ResolvingMetadata } from "next";
 import { Suspense } from "react";
-import { isFilter } from "../util";
 import ProjectContent from "./ProjectContent";
 import directus from "app/(website)/directus";
 import { readItem, readItems } from "@directus/sdk";
+import PostGrid from "app/(website)/aktuelles/blog/PostGrid";
 
 export const revalidate = 120;
 
@@ -40,6 +40,7 @@ export async function generateMetadata(props: Props, parent?: ResolvingMetadata)
     const id = params.slug;
 
     const project = await getProject(id);
+    const posts = await getPosts(id);
 
     return {
         title: `${project.title} - Potsdamer Bürgerstiftung`,
@@ -53,12 +54,17 @@ export default async function ProjectPage(
 ) {
     const params = await props.params;
     const project = getProject(params.slug);
+    const posts = getPosts(params.slug);
 
     return (
         <>
-            {/* @ts-ignore-error */}
             <Suspense>
                 <ProjectContent promise={project} />
+                <div className="container mx-auto px-4 mb-10 mt-10">
+                    <h1 className="text-3xl font-bold mb-10 font-header">Aktuelles zum Projekt</h1>
+                    {/* @ts-ignore-error */}
+                    <PostGrid promise={posts} />
+                </div>
             </Suspense>
         </>
     )
