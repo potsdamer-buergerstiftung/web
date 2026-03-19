3f5c49d588c93bfc155a8a7e3b770d80a7f25bf7
Noël Sigmunczyk <65312133+linlogge@users.noreply.github.com>
2026-03-19 16:53:14 +0100

    Run codemod to upgrade to 16

--- apps/inselbuehne/next.config.js
+++ apps/inselbuehne/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    serverActions: true,
-  },
   async rewrites() {
     return [
       {
