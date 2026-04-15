ea3eb1d38eef4f70fbab1555d9a41fa57dcdb634
Noël Sigmunczyk <65312133+linlogge@users.noreply.github.com>
2026-04-15 15:34:51 +0200

    Fix case conflicts: lowercase components

--- apps/website/components/Header/state.ts
+++
@@ -1,7 +0,0 @@
-import { atom } from "jotai";
-
-export const headerExpanded = atom(false);
-export const headerBackgroundHeight = atom(0);
-export const mobileMenuOpen = atom(false);
-export const quickDonateDrawerOpen = atom(false);
-export const quickDonationFormLoaded = atom(false);
