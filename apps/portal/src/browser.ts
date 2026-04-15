18f788a201974bd0a66818955c46f68fd2d3b631
Noël Sigmunczyk <65312133+linlogge@users.noreply.github.com>
2026-04-15 08:55:21 +0200

    Split up portal sdk into client, server and utils modules

---
+++ apps/portal/src/browser.ts
@@ -0,0 +1,8 @@
+"use client";
+
+import { createDirectus, rest, staticToken } from "@directus/sdk";
+import { Schema } from "./types/directus";
+
+const browserClient = createDirectus<Schema>("https://portal.potsdamer-buergerstiftung.org").with(rest());
+
+export default browserClient;
