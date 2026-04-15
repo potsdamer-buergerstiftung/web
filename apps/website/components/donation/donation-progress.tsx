ff3971dfa6578c764acce7411bcdfcb94b172bac
Noël Sigmunczyk <65312133+linlogge@users.noreply.github.com>
2026-04-15 11:18:36 +0200

    Revert styling the donation progress items

--- apps/website/components/donation/donation-progress.tsx
+++ apps/website/components/donation/donation-progress.tsx
@@ -2,45 +2,22 @@
 
 import { useStepper, Stepper } from "./steps";
 import { cn } from "@/lib/utils";
-import { cva, VariantProps } from "class-variance-authority";
 
-type DonationProgressIndicatorProps = {
+interface DonationProgressIndicatorProps {
   className?: string;
-  orientation?: "vertical" | "horizontal";
-} & VariantProps<typeof tabsListVariants>;
+}
 
-const tabsListVariants = cva(
-  "group/tabs-list inline-flex w-fit items-center justify-center rounded-lg p-[3px] text-muted-foreground group-data-horizontal/tabs:h-8 group-data-vertical/tabs:h-fit group-data-vertical/tabs:flex-col data-[variant=line]:rounded-none",
-  {
-    variants: {
-      variant: {
-        default: "bg-muted",
-        line: "gap-1 bg-transparent",
-      },
-    },
-    defaultVariants: {
-      variant: "default",
-    },
-  }
-)
-
-export function DonationProgressIndicator({ className, variant, orientation = "vertical" }: DonationProgressIndicatorProps) {
+export function DonationProgressIndicator({ className }: DonationProgressIndicatorProps) {
   const stepper = useStepper();
 
   return (
-    <div data-orientation={orientation} className={cn("group/tabs flex gap-2 data-horizontal:flex-col", className)}>
-      <Stepper.List className={cn(tabsListVariants({ variant }))}>
+    <div className={cn("hidden lg:flex flex-col", className)}>
+      <Stepper.List className="flex flex-col">
         {stepper.state.all.map((step) => (
           <Stepper.Item key={step.id} step={step.id}>
-            <Stepper.Trigger className={cn(
-              "relative inline-flex h-[calc(100%-1px)] flex-1 items-center justify-center gap-1.5 rounded-md border border-transparent px-1.5 py-0.5 text-sm font-medium whitespace-nowrap text-foreground/60 transition-all group-data-vertical/tabs:w-full group-data-vertical/tabs:justify-start hover:text-foreground focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 focus-visible:outline-1 focus-visible:outline-ring disabled:pointer-events-none disabled:opacity-50 has-data-[icon=inline-end]:pr-1 has-data-[icon=inline-start]:pl-1 dark:text-muted-foreground dark:hover:text-foreground group-data-[variant=default]/tabs-list:data-active:shadow-sm group-data-[variant=line]/tabs-list:data-active:shadow-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
-              "group-data-[variant=line]/tabs-list:bg-transparent group-data-[variant=line]/tabs-list:data-active:bg-transparent dark:group-data-[variant=line]/tabs-list:data-active:border-transparent dark:group-data-[variant=line]/tabs-list:data-active:bg-transparent",
-              "data-active:bg-background data-active:text-foreground dark:data-active:border-input dark:data-active:bg-input/30 dark:data-active:text-foreground",
-              "after:absolute after:bg-foreground after:opacity-0 after:transition-opacity group-data-horizontal/tabs:after:inset-x-0 group-data-horizontal/tabs:after:bottom-[-5px] group-data-horizontal/tabs:after:h-0.5 group-data-vertical/tabs:after:inset-y-0 group-data-vertical/tabs:after:-right-1 group-data-vertical/tabs:after:w-0.5 group-data-[variant=line]/tabs-list:data-active:after:opacity-100",
-              className
-            )}>
+            <Stepper.Trigger className="inline-flex flex-row py-1.5 relative group hover:text-primary data-[status=active]:text-primary">
               <Stepper.Indicator className="absolute top-1 left-1 bottom-1 w-0.5 bg-primary opacity-0 data-[status=active]:opacity-100 group-data-[status=completed]:opacity-100 group-data-[status=completed]:bg-primary/50" />
-              <Stepper.Title className="pl-3 text-sm font-medium data-[status=active]:text-primary group-data-[status=inactive]:text-muted-foreground">
+              <Stepper.Title className="pl-3 text-sm text-start font-medium data-[status=active]:text-primary group-data-[status=inactive]:text-muted-foreground">
                 {step.title}
               </Stepper.Title>
             </Stepper.Trigger>
